fbc2e4ba83b5656db57e59a7baeae1394beeffed
acadia84 <acadia84@gmail.com>
2024-09-21 16:08:22 +0800

    5

--- lesson2/main2.py
+++ lesson2/main2.py
@@ -6,7 +6,7 @@ count=0
 def mycallback(t:Timer):
     global count 
     count=count +1 
-    print(f"目前mycallback被執行:{count}次")
+    #print(f"目前mycallback被執行:{count}次")
     if count>=10:
         t.deinit()
 
